8d67cf2baa03c6bbc9104b05d15e786389c62c8c
Jacob Mishkin <user@MacBook-Pro.local>
2017-03-29 11:32:37 -0400

    added HTML to project

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Mother Fucking dachshunds</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+	<link href="dist/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+	<header>
+	<h1>Mother Fucking Dachshunds</h1>
+</header>
+<div class="main">
+		<div class="wrap">
+				<div class="hero">
+						<div class="flickr-img"></div>
+						<img id="images" src="" alt="Mother Fucking Dachshunds">
+					<div class="btn-wrap">
+						<button class="btn">click here</button>
+					</div>	
+				</div>
+		</div>
+</div>
+<script
+  src="https://code.jquery.com/jquery-3.2.0.min.js"
+  integrity="sha256-JAW99MJVpJBGcbzEuXk4Az05s/XyDdBomFqNlM3ic+I="
+  crossorigin="anonymous"></script>
+<script type="text/javascript" src="dist/js/app.min.js"></script>
+</body>
+</html>
